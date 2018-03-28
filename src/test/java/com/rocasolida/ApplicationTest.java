5fb663afc852806427f0f4b22a426a5eae28d92d
Bruno Hortelano <bhortelano@callistech.com>
2018-03-28 08:48:35 -0300

    - Mas cosas

---
+++ src/test/java/com/rocasolida/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.rocasolida;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+	@Test
+	public void test() {
+
+		String pust1 = "a";
+		String result = "a";
+
+		assertEquals(pust1, result);
+	}
+
+}
