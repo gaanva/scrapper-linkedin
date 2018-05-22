dcb3d837ea8aec3a419f7e1be5400486db57a817
Brunoli <Brunoli@DESKTOP-1OST3QF>
2018-05-22 00:05:50 -0300

    - Gilada

--- src/main/java/com/rocasolida/scrap/util/DriverFinder.java
+++ src/main/java/com/rocasolida/scrap/util/DriverFinder.java
@@ -9,6 +9,8 @@ import java.util.stream.Stream;
 
 public class DriverFinder {
 
+	public static String DRIVERS_BINARIES = "./drivers/binaries";
+
 	public static String findChromeDriver(String os) {
 		return findDriver("chromedriver", os);
 	}
@@ -16,19 +18,17 @@ public class DriverFinder {
 	public static String findFirefoxDriver(String os) {
 		if (os.toLowerCase().contains("windows")) {
 			return findDriver("marionette", os);
+		} else if (os.toLowerCase().contains("mac")) {
+			return findDriver("geckodriver", "osx");
 		} else {
-			if (os.toLowerCase().contains("mac")) {
-				return findDriver("geckodriver", "osx");
-			}
 			return findDriver("geckodriver", os);
 		}
 	}
 
 	private static String findDriver(final String name, String os) {
-		Path start = Paths.get("./drivers/binaries");
+		Path start = Paths.get(DRIVERS_BINARIES);
 		int maxDepth = 10;
-		try (Stream<Path> stream = Files.find(start, maxDepth, (path, attr) -> String.valueOf(path).contains(name)
-				&& String.valueOf(path).contains(os.toLowerCase()))) {
+		try (Stream<Path> stream = Files.find(start, maxDepth, (path, attr) -> String.valueOf(path).contains(name) && String.valueOf(path).contains(os.toLowerCase()))) {
 			Iterator<Path> it = stream.iterator();
 			while (it.hasNext()) {
 				Path path = it.next();
