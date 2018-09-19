470c542fc5b1d007e10b3a085ea6cbcf4cd4eac8
Brunoli <Brunoli@DESKTOP-1OST3QF>
2018-09-19 07:16:05 -0300

    mas cosas

--- src/test/java/com/rocasolida/FacebookPageTest.java
+++ src/test/java/com/rocasolida/FacebookPageTest.java
@@ -189,24 +189,24 @@ public class FacebookPageTest {
 	//
 	// }
 
-	// @Test
-	// public void larretaPost() throws Exception {
-	// FacebookScrap fs = new FacebookScrap(Driver.from(dt, OS), DEBUG);
-	// Publication pub = fs.obtainPostInformation("horaciorodriguezlarreta", "312245116021051", null, null, 1000, CommentsSort.NEW);
-	// fs.quit();
-	//
-	// System.out.println("Cantidad mensajes: " + pub.getComments().size());
-	// assertTrue(pub.getComments().size() > 50);
-	//
-	// }
-	@Test
-	public void larretaPost() throws Exception {
-		FacebookScrap fs = new FacebookScrap(Driver.from(dt, OS), DEBUG);
-		Publication pub = fs.obtainPostInformation("clarincom", "2136741736360921", 1536431726L, 1536436824L, 1000, CommentsSort.NEW);
-		fs.quit();
-
-		System.out.println("Cantidad mensajes: " + pub.getComments().size());
-		assertTrue(pub.getComments().size() == 0);
-
-	}
+	 @Test
+	 public void larretaPost() throws Exception {
+	 FacebookScrap fs = new FacebookScrap(Driver.from(dt, OS), DEBUG);
+	 Publication pub = fs.obtainPostInformation("horaciorodriguezlarreta", "10156879212501019", null, null, 1000, CommentsSort.NEW);
+	 fs.quit();
+	
+	 System.out.println("Cantidad mensajes: " + pub.getComments().size());
+	 assertTrue(pub.getComments().size() > 50);
+	
+	 }
+//	@Test
+//	public void larretaPost() throws Exception {
+//		FacebookScrap fs = new FacebookScrap(Driver.from(dt, OS), DEBUG);
+//		Publication pub = fs.obtainPostInformation("clarincom", "2136741736360921", 1536431726L, 1536436824L, 1000, CommentsSort.NEW);
+//		fs.quit();
+//
+//		System.out.println("Cantidad mensajes: " + pub.getComments().size());
+//		assertTrue(pub.getComments().size() == 0);
+//
+//	}
 }
