57bf1e9f2acb30b4eeb1a13d639f48ff3c4ec5fb
Antornio <gaanva@gmail.com>
2019-05-07 18:36:34 -0300

    agregando atributos para poder completar la data de los gráficos.

--- src/main/java/com/rocasolida/scrapperfacebook/entities/Page.java
+++ src/main/java/com/rocasolida/scrapperfacebook/entities/Page.java
@@ -6,9 +6,9 @@ import lombok.Data;
 
 @Data
 public class Page {
-
 	private String name;
 	private List<Publication> publications;
 	private Long likes;
-	private Long followers;
+	private Long followers; //fans
+	private long lastUpdated_utime; //last updated utime.
 }
--- src/main/java/com/rocasolida/scrapperfacebook/entities/Publication.java
+++ src/main/java/com/rocasolida/scrapperfacebook/entities/Publication.java
@@ -9,13 +9,23 @@ public class Publication {
 
 	private String id;
 	private String url;
-	private String owner;
-	private Long uTime;
+	private String owner; //Si es distinto al perfil o pagina => "Posts" sino "User Post" 
+	private Long uTime; //con esto ya se puede calcular hora y dia del post.
 	private String titulo;
 	private Integer cantShare;
 	private Integer cantReproducciones;
+	/** TODO
+	 * agregar: like, love, haha, wow, sad, angry 
+	 */
 	private Integer cantLikes;
 	private Integer cantComments;
 	private List<Comment> comments;
+	/** TODO
+	 * agregar:
+	 * Post_type: link, photo, video, live video.
+	 * cant_reactions
+	 * negative_reactions
+	 * sponsored? => ver como lo extraemos...
+	 */
 
 }
